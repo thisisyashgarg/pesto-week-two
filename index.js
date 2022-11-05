8353e0ebc4f292af8204699ca175fa36684d9d26
YashGarg220 <yash.garg2702@gmail.com>
2022-11-05 22:31:05 +0530

    learned about flexbox

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ window.addEventListener("click", (event) => {
     event.stopPropagation();
     const styles = getComputedStyle(event.target);
     const str = `<div> font size : ${styles.fontSize}, width: ${styles.width} </div>`;
-    document.getElementById("px").innerHTML = str;
+    document.getElementById("result").innerHTML = str;
 }) ;
